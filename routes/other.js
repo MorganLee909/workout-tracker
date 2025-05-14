782d917d1cb2ca36d510adcdc09111ee7477e29f
Lee Morgan <lee@leemorgan.dev>
2025-05-14 16:22:59 -0400

    Add logo.

--- routes/other.js
+++ routes/other.js
@@ -2,4 +2,5 @@ export default (app)=>{
     app.get("/", (req, res)=>res.sendFile(`${global.cwd}/views/index.html`));
     app.get("/index.js", (req, res)=>res.sendFile(`${global.cwd}/views/build/index.js`));
     app.get("/index.css", (req, res)=>res.sendFile(`${global.cwd}/views/build/index.css`));
+    app.get("/logo.svg", (req, res)=>res.sendFile(`${global.cwd}/views/logo.svg`));
 }
