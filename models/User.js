f4ea3059c7a7c017febde503b5c0b73be904613c
Lee Morgan <lee@leemorgan.dev>
2025-05-14 13:08:48 -0400

    Create the user model.

---
+++ models/User.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const UserSchema = mongoose.Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    workouts: [{
+        name: {
+            type: String,
+            required: true
+        },
+        exercises: {
+            type: [String],
+            required: false
+        }
+    }],
+    uuid: {
+        type: String,
+        required: true
+    }
+});
+
+export default mongoose.model("user", UserSchema);
