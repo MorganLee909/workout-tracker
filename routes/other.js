b0f089abe0624afb1d38a725f8157485bd0bd60b
Lee Morgan <lee@leemorgan.dev>
2025-05-17 13:35:14 -0400

    Create route to serve manifest.json.

--- routes/other.js
+++ routes/other.js
@@ -3,4 +3,5 @@ export default (app)=>{
     app.get("/index.js", (req, res)=>res.sendFile(`${global.cwd}/views/build/index.js`));
     app.get("/index.css", (req, res)=>res.sendFile(`${global.cwd}/views/build/index.css`));
     app.get("/logo.svg", (req, res)=>res.sendFile(`${global.cwd}/views/logo.svg`));
+    app.get("/manifest.json", (req, res)=>res.sendFile(`${global.cwd}/views/manifest.json`));
 }
