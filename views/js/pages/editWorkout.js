bd810f42ef068cb9c20faafa9bc3ada89ae4c1e4
Lee Morgan <lee@leemorgan.dev>
2025-05-19 17:39:37 -0400

    Add code to delete or change order or exercises in a workout.

--- views/js/pages/editWorkout.js
+++ views/js/pages/editWorkout.js
@@ -21,8 +21,28 @@ export default {
 
         for(let i = 0; i < exercises.length; i++){
             const exercise = template.cloneNode(true);
+            exercise.setAttribute("data-id", exercises[i]._id);
             exercise.querySelector("p").textContent = exercises[i].name;
+            exercise.querySelector(".editWorkoutDelete").addEventListener("click", ()=>{
+                container.removeChild(exercise);
+            });
+            exercise.querySelector(".workoutMoveUp").addEventListener("click", (e)=>{
+                this.moveExercise(e.target.parentElement.parentElement, true);
+            });
+            exercise.querySelector(".workoutMoveDown").addEventListener("click", (e)=>{
+                this.moveExercise(e.target.parentElement.parentElement, false);
+            });
             container.appendChild(exercise);
+        }
+    },
+
+    moveExercise: function(elem, moveUp){
+        const otherElem = moveUp ? elem.previousElementSibling : elem.nextElementSibling;
+
+        if(moveUp){
+            elem.parentElement.insertBefore(elem, otherElem);
+        }else{
+            elem.parentElement.insertBefore(otherElem, elem);
         }
     }
 }
